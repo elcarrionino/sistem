f21ed8500420441ebde0d2f4ebc1d8d20044e092
Oscar Clevorio Campos Salvatierra <camsalos@hotmail.com>
2017-12-05 13:20:40 -0500

    Cuarto commit

--- info.php
+++ info.php
@@ -3,5 +3,6 @@
 	echo "UNIVERSIDAD NACIONAL DANIEL ALCIDES CARRION";
 	echo "FACULTAD DE INGENIERIA"
 	echo "Escuela de Ingeniería de Sistemas y Computación"
+	echo "Ing. Oscar C. campos Salvatierra"
 	phpinfo();
 ?>
