205dea605722db83cec8b136690fccca7da38e6e
elcarrionino <34180587+elcarrionino@users.noreply.github.com>
2017-12-05 20:25:26 -0500

    onceavo commit

--- info.php
+++ info.php
@@ -3,5 +3,6 @@
 	echo "FACULTAD DE INGENIERIA"
 	echo "Escuela de Formación Profesional de Sistemas y Computación"
 	echo "Ing. Oscar C. campos Salvatierra"
+	echo "Cerro de Pasco - 2017"
 	phpinfo();
 ?>
