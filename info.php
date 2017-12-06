9a1d7b71c590a83c8f4e59fa9f2df8264879c88d
elcarrionino <34180587+elcarrionino@users.noreply.github.com>
2017-12-05 20:03:45 -0500

    Noveno commit

--- info.php
+++ info.php
@@ -1,4 +1,5 @@
 ﻿<?php
+	echo "Hola mundo ...!";	
 	echo "UNIVERSIDAD NACIONAL DANIEL ALCIDES CARRION";
 	echo "FACULTAD DE INGENIERIA"
 	echo "Escuela de Formación Profesional de Sistemas y Computación"
