d21a05ef0acadc2927c1505f4ddd4ced73ad26c2
elcarrionino <34180587+elcarrionino@users.noreply.github.com>
2017-12-05 20:26:44 -0500

    sexto commit

--- info.php
+++ info.php
@@ -1,7 +1,8 @@
 <?php
+	echo "123";
 	echo "UNIVERSIDAD NACIONAL DANIEL ALCIDES CARRION";
-	echo "FACULTAD DE INGENIERIA"
-	echo "Escuela de Ingeniería de Sistemas y Computación"
-	echo "Ing. Oscar C. campos Salvatierra"
+	echo "FACULTAD DE INGENIERIA";
+	echo "Escuela de Ingeniería de Sistemas y Computación";
+	echo "Ing. Oscar C. campos Salvatierra";
 	phpinfo();
 ?>
